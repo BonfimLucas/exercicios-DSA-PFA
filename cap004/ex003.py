2d454eeee2fe77fe12c0dcbca0197438fce25cce
Lucas Bonfim <lucasbonfim604@gmail.com>
2022-10-23 19:52:16 -0300

    exercicios python

---
+++ cap004/ex003.py
@@ -0,0 +1,6 @@
+import numpy as np 
+
+matrix = [[1, 2],[3,4],[5,6],[7,8]]
+matrix_transposta = np.array(matrix).T
+
+print(matrix_transposta)
---
+++ cap004/ex004.py
@@ -0,0 +1,11 @@
+lista = [0, 1, 2, 3, 4]
+quadrado = []
+
+def elevar_quadrado(list):
+    for i in lista:
+        quadrado.append(i **2 )
+
+
+
+
+
